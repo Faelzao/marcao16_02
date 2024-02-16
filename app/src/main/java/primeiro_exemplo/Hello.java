06e8259a9d7243a40346536b90e7d3f312d05e79
Faelzao <rrafa8982@gmail.com>
2024-02-16 12:27:01 +0000

    marcao

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -1,11 +1,40 @@
 package primeiro_exemplo;
 
+import primeiro_exemplo.modelos.Pessoa ;
+
+
 public class Hello {
     public static void main(String[] args) {
         int cont = 10;
         String nome = "Marco";
         System.out.println("Olá Mundo!!!");
 
-        System.out.println(cont + " : " + nome);
+        System.out.println(nome + " é um  " + cont);
+
+        Pessoa p1 = new Pessoa();
+        Pessoa p2 = new Pessoa();
+        Pessoa p3 = new Pessoa();
+        Pessoa p4 = new Pessoa();
+        
+        p1.nome = "Marco";
+        p1.idade = 43 ;
+    
+        p2.nome = "Rafael";
+        p2.idade = 17;
+
+        p3.idade =  p1.idade +  p2.idade;
+        System.out.println("Seu professor é o  " + p1.nome + ".  Ele que tem que idade?   "  );
+
+        System.out.println( + p1.idade);
+
+        System.out.println("O Aluno é o  " + p2.nome + ".  Ele que tem que idade?   "  );
+
+        System.out.println( + p2.idade);
+
+        System.out.println("Juntando as idade eles tem    "  );
+        System.out.println( +  p3.idade);
+
+        System.out.println(p1.verificarMaioridade()  );
+        System.out.println(p2.verificarMaioridade()  );
     }
 }
---
+++ app/src/main/java/primeiro_exemplo/modelos/Pessoa.java
@@ -0,0 +1,16 @@
+package primeiro_exemplo.modelos;
+
+public class Pessoa {
+    //atributos de uma classe
+    public String nome;
+    public int idade;
+   
+   
+   
+    //Métodos
+  public boolean verificarMaioridade(){
+    return this.idade >= 18;
+  }
+    
+}
+
